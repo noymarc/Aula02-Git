4b769543050f9dc36337a6fe748adcff323efae6
noymarc <ph898033@gmail.com>
2026-03-05 20:28:17 -0300

    classe italiano

--- Hello.java
+++ Hello.java
@@ -2,7 +2,10 @@ package Aula02;
 
 public class Hello {
     public static void main(String[] args) {
+       Hello.falar();
+       Italiaano.falar();
+    }
+    public static void falar(){
         System.out.println("Hello World!");
     }
-    
 }
---
+++ Italiaano.java
@@ -0,0 +1,7 @@
+package Aula02;
+
+public class Italiaano {
+    public static void falar() {
+        System.out.println("Ciao Mondo!");
+    }
+}
